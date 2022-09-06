36be9c120752fb3cc67ce0979285c23c7c4d54e7
Susan Buck <susanbuck@fas.harvard.edu>
2022-09-06 17:43:32 +0000

    Initialized P1

---
+++ p1/index.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang='en'>
+<head>
+
+    <title>Project 1</title>
+    <meta charset='utf-8'>
+    <link href=data:, rel=icon>
+
+</head>
+<body>
+
+<h1>Project 1</h1>
+
+<h2>Mechanics</h2>
+<ul>
+    <li>...<
+
+</body>
+</html>
