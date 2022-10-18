c4be21ee9abf921fc7c7c410dd49c66153029457
Susan Buck <susanbuck@fas.harvard.edu>
2022-10-18 00:51:38 +0000

    Week 8 related code

--- practice/vowel-count.php
+++ practice/vowel-count.php
@@ -19,3 +19,14 @@ var_dump(vowelCount('apple')); # Should yield 2
 var_dump(vowelCount('lynx')); # Should yield 0
 var_dump(vowelCount('hi')); # Should yield 1
 var_dump(vowelCount('mississippi')); # Should yield 4
+
+
+/*
+The following is an alternative solution to the vowel count function using
+regular expressions (which were briefly discussed in the Week 8 Part 1 lecture video)
+*/
+function vowelCount2($word)
+{
+    preg_match_all('/[aeiou]/i', $word, $matches);
+    return count($matches[0]);
+}
